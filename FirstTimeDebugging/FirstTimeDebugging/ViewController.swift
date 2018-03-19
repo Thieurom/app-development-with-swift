1c33da430bc374ad316faab0ba43c8e161587bcf
Doan Le Thieu <thieurom@gmail.com>
2018-03-19 13:33:53 +0700

    Lab 1.6: Debug Your First App

---
+++ FirstTimeDebugging/FirstTimeDebugging/ViewController.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let sample = "sample"
+        print(sample)
+        
+        if true {
+            print("Will this line of code ever be reached?")
+            someMethod()
+        }
+        
+        
+    }
+    
+    func someMethod() {
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
