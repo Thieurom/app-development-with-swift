229229cdba0ff08b6f5cfe765ad6b5c618d19bba
Doan Le Thieu <thieurom@gmail.com>
2018-03-20 13:02:51 +0700

    Lesson 2.9: Controls In Action

---
+++ CommonInputControls/CommonInputControls/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  CommonInputControls
+//
+//  Created by Doan Le Thieu on 3/20/18.
+//  Copyright © 2018 Doan Le Thieu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var toggle: UISwitch!
+    @IBOutlet weak var slider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        print("Button was tapped")
+        
+        if toggle.isOn {
+            print("The switch is on!")
+        } else {
+            print("The switch is off.")
+        }
+        
+        print("The slider is set to \(slider.value)")
+    }
+    
+    @IBAction func switchToggled(_ sender: UISwitch) {
+        if sender.isOn {
+            print("Switch is on!")
+        } else {
+            print("Switch is off.")
+        }
+    }
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
+    
+    @IBAction func keyboardReturnKeyTapped(_ sender: UITextField) {
+        if let text = sender.text {
+            print(text)
+        }
+    }
+    
+    @IBAction func textChanged(_ sender: UITextField) {
+        if let text = sender.text {
+            print(text)
+        }
+    }
+    
+    @IBAction func respondToTapGesture(_ sender: UITapGestureRecognizer) {
+        let location = sender.location(in: view)
+        print(location)
+    }
+}
+
