add644fb7e45adf79f36ebf66784375791b002fa
Doan Le Thieu <thieurom@gmail.com>
2018-03-21 20:07:39 +0700

    Lesson 3.6: Segues and Navigation Controllers

---
+++ TrafficSegues/TrafficSegues/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Doan Le Thieu on 3/21/18.
+//  Copyright © 2018 Doan Le Thieu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var segueSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func yellowButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
+    }
+    
+    @IBAction func greenButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
+    }
+}
+
