7ec6ef48b3a2258ed2c151de445f412dc405c9c1
Doan Le Thieu <thieurom@gmail.com>
2018-03-21 23:30:29 +0700

    Lab 3.8: Order of Events

---
+++ OrderOfEvents/OrderOfEvents/MiddleViewController.swift
@@ -0,0 +1,67 @@
+//
+//  MiddleViewController.swift
+//  OrderOfEvents
+//
+//  Created by Doan Le Thieu on 3/21/18.
+//  Copyright © 2018 Doan Le Thieu. All rights reserved.
+//
+
+import UIKit
+
+class MiddleViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    var eventNumber: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let existingText = label.text {
+            label.text = "\(existingText)\nEvent number \(eventNumber) was viewDidLoad"
+            eventNumber += 1
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let existingText = label.text {
+            label.text = "\(existingText)\nEvent number \(eventNumber) was viewWillAppear"
+            eventNumber += 1
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let existingText = label.text {
+            label.text = "\(existingText)\nEvent number \(eventNumber) was viewDidAppear"
+            eventNumber += 1
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if let existingText = label.text {
+            label.text = "\(existingText)\nEvent number \(eventNumber) was viewWillDisappear"
+            eventNumber += 1
+        }
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        if let existingText = label.text {
+            label.text = "\(existingText)\nEvent number \(eventNumber) was viewDidDisappear"
+            eventNumber += 1
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
