c2b85e2847e84ee2ab5d02485379c77075475575
Doan Le Thieu <thieurom@gmail.com>
2018-04-03 15:38:52 +0700

    Lesson 5.3: Practical Animation

---
+++ MusicWireframe/MusicWireframe/ViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  MusicWireframe
+//
+//  Created by Doan Le Thieu on 4/3/18.
+//  Copyright © 2018 Doan Le Thieu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var albumImageView: UIImageView!
+    @IBOutlet weak var reverseBackground: UIView!
+    @IBOutlet weak var playPauseBackground: UIView!
+    @IBOutlet weak var forwardBackground: UIView!
+    @IBOutlet weak var reverseButton: UIButton!
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+    
+    var isPlaying: Bool = true {
+        didSet {
+            if isPlaying {
+                playPauseButton.setImage(UIImage(named: "pause")!, for: .normal)
+            } else {
+                playPauseButton.setImage(UIImage(named: "play")!, for: .normal)
+            }
+        }
+    }
+    
+    @IBAction func playPauseButtonTapped(_ sender: UIButton) {
+        if isPlaying {
+            UIView.animate(withDuration: 0.5) {
+                self.albumImageView.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+            }
+        } else {
+            UIView.animate(withDuration: 0.5) {
+                self.albumImageView.transform = CGAffineTransform.identity
+            }
+        }
+        
+        isPlaying = !isPlaying
+    }
+    
+    @IBAction func touchedUpInside(_ sender: UIButton) {
+        let buttonBackground: UIView
+        
+        switch sender {
+        case reverseButton:
+            buttonBackground = reverseBackground
+        case playPauseButton:
+            buttonBackground = playPauseBackground
+        case forwardButton:
+            buttonBackground = forwardBackground
+        default:
+            return
+        }
+        
+        UIView.animate(withDuration: 0.25, animations: {
+            buttonBackground.alpha = 0.0
+            buttonBackground.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+            sender.transform = CGAffineTransform.identity
+        }) { (_) in
+            buttonBackground.transform = CGAffineTransform.identity
+        }
+    }
+    
+    @IBAction func touchedDown(_ sender: UIButton) {
+        let buttonBackground: UIView
+        
+        switch sender {
+        case reverseButton:
+            buttonBackground = reverseBackground
+        case playPauseButton:
+            buttonBackground = playPauseBackground
+        case forwardButton:
+            buttonBackground = forwardBackground
+        default:
+            return
+        }
+        
+        UIView.animate(withDuration: 0.25) {
+            buttonBackground.alpha = 0.3
+            sender.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        reverseBackground.layer.cornerRadius = 35.0
+        reverseBackground.clipsToBounds = true
+        reverseBackground.alpha = 0.0
+        
+        playPauseBackground.layer.cornerRadius = 35.0
+        playPauseBackground.clipsToBounds = true
+        playPauseBackground.alpha = 0.0
+        
+        forwardBackground.layer.cornerRadius = 35.0
+        forwardBackground.clipsToBounds = true
+        forwardBackground.alpha = 0.0
+    }
+
+
+
+}
+
